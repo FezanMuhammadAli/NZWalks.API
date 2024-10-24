6fd4d7691635f61f7bbfed3ed583755a5508e7ce
Fezan Muhammad Ali <bsf2101030@ue.edu.pk>
2024-10-24 23:57:35 +0500

    Regions: GetAll() refactored GetByName(),GetByCode(),GetById() added added somenew methods for better approach

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
@@ -13,24 +14,51 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController: ControllerBase
     {
-        // GET ALL REGIONS
-        // Get: https://localhost:portnumber/api/regions
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET ALL REGIONS          #1
         [HttpGet]
         public IActionResult GetAll(){
-            var regions=new List<Region>{
-                new Region{
-                    Id=Guid.NewGuid(),
-                    Name="Auckland Region",
-                    Code="AKL",
-                    RegionImageUrl="https://www.pexels.com/photo/auckland-skyline-with-sky-tower-and-ocean-view-29015601/"
-                },
-                new Region{
-                    Id=Guid.NewGuid(),
-                    Name="Wellington Region",
-                    Code="WLG",
-                    RegionImageUrl="https://www.pexels.com/photo/group-hiking-adventure-in-wellington-new-zealand-29049909/"
-                }
-            };
+            var regions=dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        // GET REGION BY ID         #2
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id){
+            // var region=dbContext.Regions.Find(id);
+            var region=dbContext.Regions.FirstOrDefault(x=>x.Id==id);
+            if(region==null){
+                return NotFound();
+            }
+            return Ok(region);
+        }
+
+        // GET REGIONS BY CODE          #3
+        [HttpGet]
+        [Route("code/{code}")]
+        public IActionResult GetByCode([FromRoute]String code){
+            var regions=dbContext.Regions.Where(x=>x.Code==code).ToList();
+            if(!regions.Any()){
+                return NotFound();
+            }
+            return Ok(regions);
+        }
+
+        // GET REGIONS BY NAME          #4
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult GetByName([FromRoute]string name){
+            var regions=dbContext.Regions.Where(x=>x.Name.Contains(name)).ToList();
+            if(!regions.Any()){
+                return NotFound();
+            }
             return Ok(regions);
         }
     }
