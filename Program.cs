6ff9eda71522e774c2e176b1b76ad4e80e44ada1
Fezan Muhammad Ali <bsf2101030@ue.edu.pk>
2024-10-24 22:10:01 +0500

    added the connectionString

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NZWalksDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
 var app = builder.Build();
 
