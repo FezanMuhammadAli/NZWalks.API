0923a4bce15d3a8eb02ec5c502d0c67641cf6f1f
engcuntio <fezanhcp@gmail.com>
2024-10-25 23:05:49 +0500

    ADDED a new DTO from Create Request

---
+++ Models/DTOs/AddRegionCreateRequestDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Models.DTOs
+{
+    public class AddRegionCreateRequestDto
+    {
+        public string Code { get; set; }
+        public String Name { get; set; }
+        public String? RegionImageUrl { get; set; }
+    }
+}
